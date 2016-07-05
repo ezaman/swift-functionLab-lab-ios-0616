9e61a6638816ceee53d79125209cb18ba74e4535
ezaman <ehsanmzaman@gmail.com>
2016-07-05 17:20:34 -0400

    writing custom swift functions and practicing

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -8,36 +8,50 @@
  * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
 
  */
-/*: question1
- ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
- */
-// write your code here
 
 
+func helloWorld () {
+    let sayHellowWorld = "Namaste, world!"
+    print(sayHellowWorld)
+}
 
-
+helloWorld()
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
-// write your code here
 
 
+func theTrueKing () {
+    
+    let jonSnow = "Jon Snow: The King in the north!"
+    print (jonSnow)
+}
 
-
+theTrueKing()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
-// write your code here
 
+func person(personsName: String) {
+    
+    let greeting = "Seven blessings, \(personsName). How are you?"
+    print(greeting)
+}
 
+person("Sandor Clegane")
+person("Cersei")
+person("Kingslayer")
 
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
-// write your code here
+let dany = "Queen Daenerys of House Targaryen, the first of her name, Queen of the Andals and ruler of the Seven Kingdoms"
+person(dany)
+var littleFinger = "Lord Baelish"
+littleFinger = "Lord Baelish, the one who fancies Sansa"
 
-
+person(littleFinger)
 
 
 /*: question5
@@ -45,9 +59,17 @@
  */
 // write your code here
 
+func bestTeamInTheNBA (){
+    
+    let bestTeam = "New York Knicks"
+    print("The best team in the NBA is the \(bestTeam), said ill-informed ESPN hosts.")
+    let secondBestTeam = "Golden State Warriors"
+    print("The best team in the NBA is the \(secondBestTeam), said ill-informed ESPN hosts.")
 
+}
 
-
+bestTeamInTheNBA()
+bestTeamInTheNBA()
 /*:
  [Solution](solution)
  */
